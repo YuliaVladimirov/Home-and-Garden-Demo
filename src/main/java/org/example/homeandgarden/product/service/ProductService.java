5b4ba50bbe265fff83aac7b2196b33c016f3cc18
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-06-11 00:50:35 +0200

    refactor: small fixes in method name

--- src/main/java/org/example/homeandgarden/product/service/ProductService.java
+++ src/main/java/org/example/homeandgarden/product/service/ProductService.java
@@ -11,7 +11,7 @@ public interface ProductService {
     PagedModel<ProductResponse> getCategoryProducts(String categoryId, BigDecimal minPrice, BigDecimal maxPrice, Integer size, Integer page, String order, String sortBy);
     PagedModel<ProductResponse> getProductsByStatus(String productStatus, Integer size, Integer page, String order, String sortBy);
     PagedModel<ProductProjectionResponse> getTopProducts(String status, Integer size, Integer page);
-    PagedModel<ProductProjectionResponse> getPendingProduct(String status, Integer days, Integer size, Integer page);
+    PagedModel<ProductProjectionResponse> getPendingProducts(String status, Integer days, Integer size, Integer page);
     ProductProfitResponse getProfitByPeriod(String period, Integer timePeriod);
     ProductResponse getProductById(String productId);
     ProductResponse addProduct(ProductCreateRequest productCreateRequest);
