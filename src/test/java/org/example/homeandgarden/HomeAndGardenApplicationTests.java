e93cdd85fbee87614b34555cf7788ea09570cc12
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-07-17 17:50:32 +0200

    tests: apply small optimizations.

--- src/test/java/org/example/homeandgarden/HomeAndGardenApplicationTests.java
+++ src/test/java/org/example/homeandgarden/HomeAndGardenApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.homeandgarden;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HomeAndGardenApplicationTests {
 
 	@Test
