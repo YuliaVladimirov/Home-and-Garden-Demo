3f6c53b7918e5b4c0fb75b9a5be64203769d98b6
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-07-24 07:29:04 +0200

    refactor: improve naming in annotations.

--- src/main/java/org/example/homeandgarden/user/dto/UserRegisterRequest.java
+++ src/main/java/org/example/homeandgarden/user/dto/UserRegisterRequest.java
@@ -27,14 +27,14 @@ public class UserRegisterRequest {
     @JsonProperty("password")
     @NotBlank(message = "Password is required")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$",
-            message = "Invalid password: Must contain at least one digit, one lowercase letter, one uppercase letter, one special character, no whitespace, and be at least 8 characters long")
+            message = "Invalid value for password: Must contain at least one digit, one lowercase letter, one uppercase letter, one special character, no whitespace, and be at least 8 characters long")
     @Schema(description = "User's password")
     private String password;
 
     @JsonProperty("confirmPassword")
     @NotBlank(message = "Confirm password field is required")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$",
-            message = "Invalid password: Must contain at least one digit, one lowercase letter, one uppercase letter, one special character, no whitespace, and be at least 8 characters long")
+            message = "Invalid value for confirm password: Must contain at least one digit, one lowercase letter, one uppercase letter, one special character, no whitespace, and be at least 8 characters long")
     @Schema(description = "Confirmation of user's password")
     private String confirmPassword;
 
