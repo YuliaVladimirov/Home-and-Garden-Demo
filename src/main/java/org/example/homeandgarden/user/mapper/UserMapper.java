6a4fd9f849588c4798c1e784ae24095fe04fc010
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-08-13 17:23:19 +0200

    refactor: apply small optimizations to mapper method.

--- src/main/java/org/example/homeandgarden/user/mapper/UserMapper.java
+++ src/main/java/org/example/homeandgarden/user/mapper/UserMapper.java
@@ -5,21 +5,17 @@ import org.example.homeandgarden.user.dto.UserRegisterRequest;
 import org.example.homeandgarden.user.dto.UserResponse;
 import org.example.homeandgarden.user.entity.User;
 import org.example.homeandgarden.user.entity.enums.UserRole;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserMapper {
 
-    private final PasswordEncoder passwordEncoder;
-
     public User createRequestToUser(
             UserRegisterRequest userRegisterRequest) {
 
         return User.builder()
                 .email(userRegisterRequest.getEmail())
-                .passwordHash(passwordEncoder.encode(userRegisterRequest.getPassword()))
                 .firstName(userRegisterRequest.getFirstName())
                 .lastName(userRegisterRequest.getLastName())
                 .userRole(UserRole.CLIENT)
